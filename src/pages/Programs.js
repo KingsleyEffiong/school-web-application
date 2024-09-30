a0f73e95eeceebb943c9403d9eca1db5841aff5f
Kingslet Effiong <kingsleyeffiong642@gmail.com>
2024-09-30 13:21:15 +0100

    updated

--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Programs() {
   return (
-    <div className='w-full h-fit bg-gray-500 p-7 overflow-hidden'>
+    <div className='w-full h-fit bg-gray-500 overflow-hidden'>
     <h1 className='pt-44 text-center text-4xl text-white animate-scaleUpCenter'>Our Programs</h1>
-    <div className='flex flex-row flex-wrap justify-center w-[80%] m-auto px-7'>
+    <div className='flex flex-row flex-wrap justify-center w-[80%] m-auto'>
       {/* First Row */}
-      <div className='flex flex-row flex-wrap  justify-around items-center m-10 px-7'>
-        <div className='w-96 m-4'>
+      <div className='flex flex-row flex-wrap  justify-around items-center m-10'>
+        <div className='w-96 m-4 px-10 md:px-0'>
           <h2 className='bg-rose-900 text-white py-4 px-5 w-fit animate-scaleUpCenter'>Creche</h2>
           <p className='text-1xl md:text-2xl animate-scaleUpCenter'>A nurturing space for the youngest learners, where care and learning go hand in hand.</p>
         </div>
@@ -17,8 +17,8 @@ function Programs() {
       </div>
       
       {/* Second Row (reverse order) */}
-      <div className='flex flex-row-reverse flex-wrap  justify-around items-center m-10 px-3'>
-        <div className='w-96 m-4'>
+      <div className='flex flex-row-reverse flex-wrap  justify-around items-center m-10'>
+        <div className='w-96 m-4 px-10 md:px-0'>
           <h2 className='bg-rose-900 text-white py-4 px-5 w-fit animate-scaleUpCenter'>Pre-School</h2>
           <p className='text-1xl md:text-2xl animate-scaleUpCenter'> Building the first steps in education through engaging activities and foundational skills</p>
         </div>
@@ -28,8 +28,8 @@ function Programs() {
       </div>
       
       {/* Third Row */}
-      <div className='flex flex-row flex-wrap  justify-around items-center m-10 px-3'>
-        <div className='w-96 m-4'>
+      <div className='flex flex-row flex-wrap  justify-around items-center m-10'>
+        <div className='w-96 m-4 px-10 md:px-0'>
           <h2 className='bg-rose-900 text-white py-4 px-5 w-fit animate-scaleUpCenter'>Grade School</h2>
           <p className='text-1xl md:text-2xl animate-scaleUpCenter'>Preparing students for academic excellence with a curriculum that promotes critical thinking and creativity.</p>
         </div>
